fb8f31eab216a314028c3d963d0b9a2e5a79ffbe
tarunkumarf22labs <119585731+tarunkumarf22labs@users.noreply.github.com>
2023-01-19 18:30:31 +0530

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import preact from "@preact/preset-vite";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [preact(), cssInjectedByJsPlugin()],
+
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(  __dirname   , "src/main.tsx"),
+      name: "tolstoyf22",
+      formats: ["es"],
+      // the proper extensions will be added
+      fileName: "tolstoyf22",
+    },
+  },
+});
